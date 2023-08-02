76c61ecde0df6154ee9be8cb3debc50b2205572f
samanthamarberger <slatham04@gmail.com>
2023-08-02 13:31:41 -0600

    Added create

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,10 +2,26 @@ class StudentsController < ApplicationController
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-    
+    def index
+        students = Student.all
+        render json: students, status: :ok
+    end
 
+    def show
+        students = Student.find(params[:id])
+        render json: students, status: :ok
+    end
+
+    def create
+        student = Student.create(student_params)
+        render json: student, status: :created
+    end
 
     private
+
+    def student_params
+        params.permit(:name, :major, :age, :instructor_id)
+    end
 
     def render_not_found_response
       render json: { error: "Student not found" }, status: :not_found
